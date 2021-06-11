8bd5f7d80553e9910d523e4109ebd0ad3353b136
Kionashi <cardozo.anibal@gmail.com>
2021-06-11 22:56:23 +0200

    create .gitignore and stuff

---
+++ src/com/ironhack/helloworld/main/Main.java
@@ -0,0 +1,15 @@
+package com.ironhack.helloworld.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String name = "Salvatore";
+
+        boolean var = true;
+        String greetings = name == "Salvatore"? "Hello Salvatore" : "WHo are you?";
+
+    }
+
+
+}
